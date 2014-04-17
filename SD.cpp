34d69b17a46180b774614e600b09aea2e8d8e4b1
AxelTB <AxelTB@teach>
2014-04-17 19:38:13 +0200

    Stuff

--- SD.cpp
+++ SD.cpp
@@ -1,11 +1,73 @@
 #include "SD.h"
 
-SD::SD()
+/** @brief write
+  *
+  * @todo: document this function
+  */
+void SD::write(uint8_t c)
 {
-    //ctor
+    this->buffer[this->bufferIndex++]=c; //Save character
+
+    if(this->bufferIndex==BLOCKSIZE) //Block full
+    {
+        uint16_t wcount=0;
+        int16_t index=this->updateIndex(); //Update index
+        FatFs.lseek(index*BLOCKSIZE); //Move to the right block
+        FatFs.write(this->buffer,BLOCKSIZE,&wcount);
+    }
 }
 
-SD::~SD()
+/** @brief ~SD
+  *
+  * @todo: document this function
+  */
+ SD::~SD()
 {
-    //dtor
+
 }
+
+/** @brief read
+  *
+  * @todo: document this function
+  */
+int16_t SD::read(void *data,size_t dimension)
+{
+
+}
+
+/** @brief printf
+  *
+  * @todo: document this function
+  */
+int16_t SD::printf(const char *format, ...)
+{
+
+}
+
+/** @brief write
+  *
+  * @todo: document this function
+  */
+int16_t SD::write(const void *data,size_t dimension)
+{
+
+}
+
+/** @brief begin
+  *
+  * @todo: document this function
+  */
+uint8_t SD::begin(uint8_t csPin)
+{
+    this->bufferIndex=0;
+}
+
+/** @brief SD
+  *
+  * @todo: document this function
+  */
+ SD::SD()
+{
+    this->bufferIndex=0;
+}
+
