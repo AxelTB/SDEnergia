f8f97941515c87b36d917cebec115ee9a7b81ee8
AxelTB <AxelTB@teach>
2014-04-17 23:12:43 +0200

    KISS Version

--- SDLogger.cpp
+++ SDLogger.cpp
@@ -1,11 +1,77 @@
 #include "SDLogger.h"
 
-SDLogger::SDLogger()
+/** @brief getError
+  *
+  * @todo: document this function
+  */
+FRESULT SDLogger::getError()
 {
-    //ctor
+    return this->lastError;
 }
 
-SDLogger::~SDLogger()
+/** @brief write
+  *
+  * @todo: document this function
+  */
+void SDLogger::write(uint8_t c)
 {
-    //dtor
+    WORD bw;
+
+    lastError=this->FatFs.write(&c,1,&bw);
+
+    //If file not open
+    if(err==FR_NOT_OPENED)
+    {
+        if(this->FatFs.open(LOGFILE)) //If file open fails give up
+            return;
+        //Reset written byte & error
+        this->lastError=0;
+        this->byteWritten=0;
+        lastError=this->FatFs.write(&c,1,&bw);
+
+    }
+    if(bw==1)
+        this->byteWritten++; //Increment byte write count
+
+    if(this->byteWritten==BLOCKSIZE)
+    {
+
+        this->FatFs.write(0,0,bw); //Finalize Write
+        this->FatFs.close(); //Close log file
+
+        this->blockN++; //Switch to next block
+
+        if(this->FatFs.open(INDEXFILE)); //Open Index
+        this->FatFs.write(&blockN,sizeof(uint16_t),&bw); //Save block index
+        //this->FatFs.read(&index,sizeof(uint16_t),&bw);
+        this->FatFs.close(); //Close index file
+
+        this->FatFs.open(LOGFILE); //Re-open logfile
+        this->FatFs.lseek(blockN*BLOCKSIZE); //Go to first free block
+    }
+}
+
+/** @brief Init SDLogger
+  *
+  * @todo: document this function
+  */
+FRESULT SDLogger::begin(unsigned char csPin)
+{
+    //Reset writtend byte & error
+    this->lastError=0;
+    this->byteWritten=0;
+
+    this->lastError=this->FatFs.begin(csPin,SPIDIVIDER);
+
+    return this->lastError;
+}
+
+/** @brief SDLogger
+  *
+  * @todo: document this function
+  */
+SDLogger::SDLogger()
+{
+    this->lastError=0;
+    this->byteWritten=0;
 }
--- SDLogger.h
+++ SDLogger.h
@@ -1,21 +1,37 @@
 #ifndef SDLOGGER_H
 #define SDLOGGER_H
 #include <pfatfs.h>
+#include <Print.h>
 
-class SDLogger
+#define SPIDIVIDER 2
+#define BLOCKSIZE 512
+
+#define LOGFILE     "log.txt"
+#define INDEXFILE   "index.ax"
+
+class SDLogger :Print
 {
 public:
     SDLogger();
 #if _SoftSPI
-    FRESULT begin (unsigned char,unsigned char,unsigned char,unsigned char);
+   //FRESULT begin (unsigned char,unsigned char,unsigned char,unsigned char);
 #else
     FRESULT begin (unsigned char);
 #endif
 
-    virtual ~SDLogger();
+    virtual void write(uint8_t);
+    FRESULT getError();
+
+
 protected:
 private:
-    uint16_t byteWritten;
+    uint16_t byteWritten,blockN;
+    FRESULT lastError;
+    PFFS FatFs;
+
+
+    FRESULT loadIndex();
+    FRESULT saveIndex();
 };
 
 #endif // SDLOGGER_H
